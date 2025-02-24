48da952feba2394933cd603e43a5c78f7187a73b
taleandtoast <taleandtoast@gmail.com>
2025-02-23 19:21:54 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log("Tale & Toast website loaded!");
+
+// You can add more interactive features here, such as:
+// - Image sliders
+// - Form submissions for orders
+// - Dynamic story previews
